9289628b60e42363ea9ffd41cd5192fa3e6b2128
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-23 11:19:29 +0530

    modified index.js in .

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const PORT = 3000;
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: 'Maxwell',
-    })
-  );
+  if (req.url === '/friends') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: 'Maxwell',
+      })
+    );
+  } else if (req.url === '/messages') {
+    // Corrected URL path
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Maxwell</li>');
+    res.write('<li>You gonna ACE the interview</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end(); // Properly close the response
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
