956f6be554b8542c4249c154e848c84f5653392e
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-24 15:56:48 +0530

    modified index.js in .

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: 'Lara',
+  },
+  {
+    id: 1,
+    name: 'Maxwell',
+  },
+  {
+    id: 2,
+    name: 'Rhea',
+  },
+];
+
 server.on('request', (req, res) => {
-  if (req.url === '/friends') {
+  const items = req.url.split('/');
+
+  if (req.method === 'POST' && items[1] === 'friends') {
+    req.on('data', (data) => {
+      const friend = data.toString();
+      console.log('Request:', friend);
+      friends.push(JSON.parse(friend));
+    });
+
+  } else if (req.method === 'GET' && items[1] === 'friends') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-
-    res.end(
-      JSON.stringify({
-        id: 1,
-        name: 'Maxwell',
-      })
-    );
-  } else if (req.url === '/messages') {
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (req.method === 'GET' && items[1] === 'messages') {
     // Corrected URL path
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
